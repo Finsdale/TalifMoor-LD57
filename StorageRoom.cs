77644e51dd8f494b3eeb1d40586dcd427c688199
Finsdale <Talif_Moor@yahoo.com>
2025-04-06 13:53:42 -0400

    adding relevant data types

---
+++ StorageRoom.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dumpster_Diving
+{
+  internal class StorageRoom
+  {
+    readonly Dictionary<Point, Tile> storageRoom;
+    StorageRoom()
+    {
+      for(var y = 0; y < 10; y++) {
+        for (var x = 0; x < 10; x++) {
+          storageRoom.Add(new Point(x,y), new Tile(new Point(x,y)));
+        }
+      }
+    }
+  }
+}
---
+++ Tile.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dumpster_Diving
+{
+  internal class Tile
+  {
+    public enum TileType
+    {
+      Obstruction,
+      Storage,
+      Entry,
+      Exit,
+      EntryAndExit,
+      Dump
+    }
+    Point Position { get; set; }
+    bool Occupied = false;
+    public TileType Type { get; set; }
+    public Tile(Point position, TileType type = TileType.Storage, bool occupied = false)
+    {
+      this.Position = position;
+      this.Type = type;
+      if(type == TileType.Obstruction) {
+        this.Occupied = true;
+      }
+      else {
+        this.Occupied = occupied;
+      }
+    }
+  }
+}
